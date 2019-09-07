b25726a2abfbf989b261280cbfd9c6a3d8506e4d
pizzaminded <pizzaminded@users.noreply.github.com>
2019-09-07 15:08:58 +0200

    [Core] Moved and refactored AfterControllerEvent

---
+++ Event/AfterControllerEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Jadob\Core\Event;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class AfterControllerEvent
+{
+
+    /**
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * AfterControllerEvent constructor.
+     * @param Response $response
+     */
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @return Response
+     */
+    public function getResponse(): Response
+    {
+        return $this->response;
+    }
+
+    /**
+     * @param Response $response
+     * @return AfterControllerEvent
+     */
+    public function setResponse(Response $response): self
+    {
+        $this->response = $response;
+        return $this;
+    }
+}
